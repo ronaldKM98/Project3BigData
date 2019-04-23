d98d9f55cb9189a7d4ca6236576debe1059a2fa9
CAMILA <camila@MacBook-Pro-de-CAMILA-2.local>
2019-04-23 07:12:23 -0500

    Removing White Spaces

--- notebooks/CleaningDatasets.scala
+++ notebooks/CleaningDatasets.scala
@@ -1,28 +1,24 @@
-val stop_words: Array[String] = Array("a", "about", "above", "across", "after", "afterwards", "again", "against", 
-"all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", 
-"amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "aren’t", 
-"around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", 
-"beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", 
-"but", "by", "call", "can", "cannot", "can’t", "co", "con", "could", "couldn’t", "cry", "de", "describe", "detail", 
-"do", "don’t", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", 
-"empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", 
-"fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", 
-"from", "front", "full", "further", "get", "give", "go", "had", "has", "hasn’t", "have", "haven’t", "he", "hence", 
-"her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", 
-"however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "it’s", "itself", 
-"keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", 
-"mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", 
-"neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", 
-"nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", 
-"ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", 
-"seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", 
-"six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", 
-"system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", 
-"thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", 
-"three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", 
-"twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", 
-"whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", 
-"wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", 
+val stop_words: Array[String] = Array("a", "about", "above", "across", "after", "afterwards", "again", "against", "all", "almost", 
+"alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amoungst", "amount", "an", "and", "another", 
+"any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "aren’t", "around", "as", "at", "back", "be", "became", "because", 
+"become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", 
+"bill", "both", "bottom", "but", "by", "call", "can", "cannot", "can’t", "co", "con", "could", "couldn’t", "cry", "de", "describe", 
+"detail", "do", "don’t", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", 
+"enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", 
+"fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", 
+"had", "has", "hasn’t", "have", "haven’t", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", 
+"him", "himself", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "it’s", 
+"itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", 
+"more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", 
+"nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", 
+"onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", 
+"rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", 
+"sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", 
+"take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", 
+"therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", 
+"thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", 
+"very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", 
+"wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", 
 "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves")
 
 
@@ -60,8 +56,7 @@ val titleRemover: StopWordsRemover = new StopWordsRemover()
     .setInputCol("title_tokenized")
     .setOutputCol("cleanTitle")
     .setStopWords(stop_words)
-
-val titleDF: DataFrame = titleRemover.transform(titleTokenized)            
+val titleDF: DataFrame = titleRemover.transform(titleTokenized)          
 //titleDF.show()
 
 
@@ -90,7 +85,6 @@ def removeSpecialChars(content: Seq[String]): String  = {
   content.filter(word => word.length > 1)
          .mkString(" ")
          .replaceAll("[^a-z\\sA-Z]", "")
-         .trim.replaceAll(" +"," ") 
 }
 val removeSpecialCharsUdf: UserDefinedFunction = udf(removeSpecialChars _)
 
@@ -102,9 +96,18 @@ def removeSingleChars(content: String): String = {
 val removeSingleCharsUdf: UserDefinedFunction = udf(removeSingleChars _)
 
 
+//Function to remove white spaces
+def removeWhiteSpaces(content: String): String = {
+  content.trim.replaceAll(" +"," ")
+}
+val removeWhiteSpacesUdf: UserDefinedFunction = udf(removeWhiteSpaces _)
+
+
 //Create new dataframe with id column and the title and content columns already clean.
 val df: DataFrame = newsDF.withColumn("cleanTitle", removeSpecialCharsUdf($"cleanTitle"))
                           .withColumn("cleanContent", removeSpecialCharsUdf($"cleanContent"))
                           .withColumn("cleanTitle", removeSingleCharsUdf($"cleanTitle"))
                           .withColumn("cleanContent", removeSingleCharsUdf($"cleanContent"))
+                          .withColumn("cleanTitle", removeWhiteSpacesUdf($"cleanTitle"))
+                          .withColumn("cleanContent", removeWhiteSpacesUdf($"cleanContent"))
 df.show()
